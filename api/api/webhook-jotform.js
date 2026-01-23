16d894270697fcf32256917acb55bc166ed902ea
claudiacornecand-jpg <claudiacornecand@gmail.com>
2026-01-23 13:34:00 -0500

    Update webhook-jotform.js

--- api/api/webhook-jotform.js
+++ api/api/webhook-jotform.js
@@ -1,11 +1,1 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    envs: {
-      hasGmailUser: !!process.env.GMAIL_USER,
-      hasGmailPass: !!process.env.GMAIL_PASS,
-      hasClientEmail: !!process.env.GCAL_CLIENT_EMAIL,
-      hasPrivateKey: !!process.env.GCAL_PRIVATE_KEY
-    }
-  });
-}
+
